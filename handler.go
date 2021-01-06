d2cbff721d3856890ef40b489a092292556566a5
Charles Durieux <charles.durieux+git@negentropy.in.net>
2021-01-06 20:17:11 +0100

    Add MapHandler function

--- handler.go
+++ handler.go
@@ -11,8 +11,14 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		target, ok := pathsToUrls[r.URL.Path]
+		if ok {
+			http.Redirect(w, r, target, 301)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	})
 }
 
 // YAMLHandler will parse the provided YAML and then return
