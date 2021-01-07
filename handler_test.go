55ce7a74c7129a235b2d4e60ed85653da1f9f2d1
Charles Durieux <charles.durieux+git@negentropy.in.net>
2021-01-07 11:11:01 +0100

    Refactor MapHandler test

--- handler_test.go
+++ handler_test.go
@@ -9,56 +9,66 @@ import (
 	"testing"
 )
 
+func sendReq(handler http.HandlerFunc, target string) (*http.Response, string) {
+	rq := httptest.NewRequest("GET", target, nil)
+	w := httptest.NewRecorder()
+	handler(w, rq)
+
+	rs := w.Result()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		panic(err)
+	}
+	return rs, string(body)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, world!")
+	})
+	return mux
+}
+
 func TestMapHandler(t *testing.T) {
-	// test handler
+	// mock redirect map
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, world!")
+	// generate handler
+	handler := MapHandler(pathsToUrls, defaultMux())
+
+	t.Run("path=\"/yaml-godoc\"", func(t *testing.T) {
+		rs, _ := sendReq(handler, "/yaml-godoc")
+
+		t.Run("chk=statusCode", func(t *testing.T) {
+			want := 301
+			got := rs.StatusCode
+			if got != want {
+				t.Errorf("status code = \"%d\"; want \"%d\"", got, want)
+			}
+		})
+
+		t.Run("chk=location", func(t *testing.T) {
+			want := pathsToUrls["/yaml-godoc"]
+			got := rs.Header.Get("Location")
+			if got != want {
+				t.Errorf("Header[\"Location\"] = \"%s\"; want \"%s\"", got, want)
+			}
+		})
 	})
 
-	handler := MapHandler(pathsToUrls, mux)
+	t.Run("path=\"/xyz\"", func(t *testing.T) {
+		_, body := sendReq(handler, "/xyz")
 
-	// test defined route
-	rq := httptest.NewRequest("GET", "/yaml-godoc", nil)
-	w := httptest.NewRecorder()
-	handler(w, rq)
-
-	rs := w.Result()
-
-	// check status code
-	sc_want := 301
-	sc_got := rs.StatusCode
-	if sc_got != sc_want {
-		t.Errorf("status code = \"%d\"; want \"%d\"", sc_got, sc_want)
-	}
-
-	// check Location
-	l_want := pathsToUrls["/yaml-godoc"]
-	l_got := rs.Header.Get("Location")
-	if l_got != l_want {
-		t.Errorf("Header[\"Location\"] = \"%s\"; want \"%s\"", l_got, l_want)
-	}
-
-	// test fallback route
-	rq = httptest.NewRequest("GET", "/test", nil)
-	w = httptest.NewRecorder()
-	handler(w, rq)
-
-	rs = w.Result()
-	body, err := ioutil.ReadAll(rs.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	// check body
-	b_want := "Hello, world!"
-	b_got := strings.TrimSpace(string(body))
-	if b_want != b_got {
-		t.Errorf("Response Body = \"%s\"; want \"%s\"", b_got, b_want)
-	}
+		t.Run("chk=body", func(t *testing.T) {
+			want := "Hello, world!"
+			got := strings.TrimSpace(body)
+			if want != got {
+				t.Errorf("Response Body = \"%s\"; want \"%s\"", got, want)
+			}
+		})
+	})
 }
